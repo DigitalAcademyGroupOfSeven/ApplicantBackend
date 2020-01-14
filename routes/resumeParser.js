23fe34ade26e3adef48cc3e5ab14e643fb6e2a01
Logan-Lim <Logan.Lim@cra-arc.gc.ca>
2020-01-14 16:35:22 -0500

    wip resume upload

--- routes/resumeParser.js
+++ routes/resumeParser.js
@@ -2,23 +2,29 @@ const express = require('express')
 const router = express.Router()
 const formidable = require('formidable')
 const fs = require('fs')
+const axios = require('axios')
 
 /* POST receive resume. */
 router.post('/', function(req, res, next) {
     new formidable.IncomingForm().parse(req, (err, fields, files) => {
         if (err) {throw err}
-
+        
         let file = files.fileKey
 
-        axios.post('http://127.0.0.1:5000/process', req.body)
-        .then((res) => {
-            console.log(`statusCode: ${res.statusCode}`)
-            console.log(res)
-        })
-        .catch((error) => {
-            console.error(error)
-        })   
+        var formData = new FormData()
+        formData.append('file', file)
+        axios.post('http://127.0.0.1:5000/process', formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
     })
+    .then((res) => {
+        console.log(res)
+    })
+    .catch((error) => {
+        console.error(error)
+    })   
+})
 });
 
 router.post('/result', function(req, res, next) {
