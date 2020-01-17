a5989ebd41396a902be74a89182a98f1bb7da96f
Logan-Lim <Logan.Lim@cra-arc.gc.ca>
2020-01-17 15:38:34 -0500

    added some more fields to applicant

--- routes/applicant/applicant.model.js
+++ routes/applicant/applicant.model.js
@@ -4,12 +4,18 @@ const Schema = mongoose.Schema
 const schema = new Schema({
     username: {type: String, unique: true, required: true},
     hash: {type: String, required:true},
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    email: {type: String, required: true},
+    name: {type: String},
+    lastName: {type: String},
+    email: {type: String},
     skills: [{type: String}],
     mobile_number: {type: String},
-    experience: [{type: String}]
+    experience: [{type: String}],
+    college_name: {type: String},
+    company_names: [{type: String}],
+    degree: [{type: String}],
+    designation: [{type: String}],
+    total_experience: {type: String},
+
 })
 
 schema.set('toJSON', {virtuals: true})
